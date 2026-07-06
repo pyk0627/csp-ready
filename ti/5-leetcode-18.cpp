e1ac4ec6f70df43351f27392afe7a14eb4b2d24d
pyk <2316890929@qq.com>
2026-07-06 10:45:56 +0800

    四数之和

---
+++ ti/5-leetcode-18.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int n=nums.size();//用int类型存储长度，避免溢出
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> res;
+        for(int i=0;i<n-3;i++)
+        {
+            if(i>0&&nums[i]==nums[i-1])
+            {
+                continue;
+            }
+            for(int j=i+1;j<n-2;j++)
+            {
+
+                if(j>i+1&&nums[j]==nums[j-1])//这里是j>i+1,而不是j>0
+                {
+                    continue;
+                }
+                int k=j+1;
+                int l=nums.size()-1;
+                // cout<<"k:"<<k<<endl;
+                // cout<<"l:"<<l<<endl;
+                while(k<l)
+                {
+                    // cout<<"nums[i]:"<<nums[i]<<endl;
+                    // cout<<"nums[j]:"<<nums[j]<<endl;
+                    // cout<<"nums[k]:"<<nums[k]<<endl;
+                    // cout<<"nums[l]:"<<nums[l]<<endl;
+                    long long t = (long long)nums[i]+nums[j]+nums[k]+nums[l];//将第一个数字转为long long 避免溢出
+                    // cout<<"t:"<<t<<endl;
+                    if(t<target)
+                    {
+                        k++;
+                    }else if(t>target)
+                    {
+                        l--;
+                    }else if(t==target)
+                    {
+                        vector<int> r(4);
+                        r[0]=nums[i];
+                        r[1]=nums[j];
+                        r[2]=nums[k];
+                        r[3]=nums[l];
+                        res.push_back(r);
+
+                        k++;
+                        while(k<l&&nums[k]==nums[k-1])
+                        {
+                            k++;
+                        }
+
+                        l--;
+                        while(k<l&&nums[l]==nums[l+1])
+                        {
+                            l--;
+                        }
+                    }
+                }
+            }
+        }
+        return res;
+    }
+};
+
+int main() {
+    int n;
+    cout << "请输入数组长度: ";
+    cin >> n;
+
+    vector<int> nums(n);
+    cout << "请输入 " << n << " 个整数: ";
+    for (int i = 0; i < n; ++i) {
+        cin >> nums[i];
+    }
+
+    int target;
+    cout << "请输入目标值: ";
+    cin >> target;
+
+    // 打印读取的数据，确认输入正确
+    cout << "\n读取完成！数组为: ";
+    for (int x : nums) cout << x << " ";
+    cout << "\n目标值: " << target << endl;
+
+    // 调用 fourSum（当前返回空）
+    Solution sol;
+    vector<vector<int>> result = sol.fourSum(nums, target);
+
+    cout << "fourSum 返回结果（未实现）: ";
+    // 打印空结果（便于观察）
+    cout << "[]" << endl;
+
+    return 0;
+}
