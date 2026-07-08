c1e1fc8e91a75f27fd7647c9afb89ad6d62c8891
pyk <2316890929@qq.com>
2026-07-08 08:58:05 +0800

    长度最小的子数组

---
+++ ti/11-lc-713.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int n = nums.size();
+        int ans=n+1;
+        int s=0;
+        int l=0;
+        for(int r=0;r<n;r++)
+        {
+            s+=nums[r];
+            while(s-nums[l]>=target)
+            {
+                s-=nums[l];
+                l++;
+            }
+            if(s>=target)
+            {
+                ans=min(ans,r-l+1);
+            }
+        }
+        if(ans<=n)
+        {
+            return ans;
+        }else
+        {
+            return 0;
+        }
+
+    }
+};
