8e208013bd10c2a781bb4b22e0d5751aec7e9144
pyk <2316890929@qq.com>
2026-07-27 16:40:09 +0800

    环形链表（二）

---
+++ ti/45-lc-142.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast!=nullptr && fast->next!=nullptr)
+        {
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast)
+            {
+                while(slow!=head)
+                {
+                    slow=slow->next;
+                    head=head->next;
+                }
+                return slow;
+            }
+        }
+        return nullptr;
+    }
+};
