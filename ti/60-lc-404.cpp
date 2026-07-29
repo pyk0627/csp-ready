29300d4fbd522f3c15213ff4b844495f23c59b92
pyk <2316890929@qq.com>
2026-07-29 10:53:28 +0800

    左叶子之和

---
+++ ti/60-lc-404.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    long long ans=0;
+public:
+    int sumOfLeftLeaves(TreeNode* root) {
+        if(root == nullptr)
+        {
+            return 0;
+        }   
+        if( root->left != nullptr &&
+            root->left->left==nullptr &&
+            root->left->right==nullptr
+        )
+        {
+            ans+=root->left->val;          
+        }
+        sumOfLeftLeaves(root->left);
+        sumOfLeftLeaves(root->right);
+        return ans;
+    }
+};
