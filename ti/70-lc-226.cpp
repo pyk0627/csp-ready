0dca39226ae5ba130dddf3174b91f2e4a8cc7857
pyk <2316890929@qq.com>
2026-07-30 13:34:40 +0800

    翻转二叉树

---
+++ ti/70-lc-226.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(root==nullptr)
+        {
+            return nullptr;
+        }
+            TreeNode* t=root->left;
+            root->left=root->right;
+            root->right=t;
+
+            root->left=invertTree(root->left);
+            root->right=invertTree(root->right);
+            return root;
+    }
+};
