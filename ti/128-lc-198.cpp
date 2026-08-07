5f70d6bffa92c0e7f4bb889f9c300149eb651082
pyk <2316890929@qq.com>
2026-08-07 14:52:37 +0800

    打家劫舍->（动态规划）

---
+++ ti/128-lc-198.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int f0=0;
+        int f1=0;
+        for(auto x:nums)
+        {
+            int f2=max(f1,f0+x);
+            f0=f1;
+            f1=f2;
+        }
+        return f1;
+    }
+};
