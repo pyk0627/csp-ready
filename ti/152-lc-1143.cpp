1ae8fba173b41e53266f6665350aaf2bbfa61304
pyk <2316890929@qq.com>
2026-08-19 09:29:26 +0800

    最长公共子序列

--- ti/152-lc-1143.cpp
+++ ti/152-lc-1143.cpp
@@ -3,18 +3,23 @@ public:
     int longestCommonSubsequence(string text1, string text2) {
         int n=text1.size();
         int m=text2.size();
+        vector<vector<int>> memo(n,vector<int>(m,-1));
         auto dfs=[&](this auto&& dfs,int i,int j)->int
         {
             if(i<0||j<0)
             {
                 return 0;
             }
+            if(memo[i][j]!=-1)
+            {
+                return memo[i][j];
+            }
             if(text1[i]==text2[j])
             {
-                return dfs(i-1,j-1)+1;
+                return memo[i][j]=dfs(i-1,j-1)+1;
             }else
             {
-                return max(dfs(i-1,j),dfs(i,j-1));
+                return memo[i][j]=max(dfs(i-1,j),dfs(i,j-1));
             }
         };
         return dfs(n-1,m-1);
